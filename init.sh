ffcdcbd93dc7aa22c4048ac22af750e4b98220e6
Philip Woods <elzairthesorcerer@gmail.com>
2014-12-03 14:23:26 -0500

    Merged lib/git.js, lib/run.js and lib/unit-test.js into lib/shell.js

--- init.sh
+++ init.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+
+# Add user
 sudo useradd -m -s /bin/false -d /home/lana lana
+
+# Give user non-sudo access to docker
 sudo gpasswd -a lana docker
 sudo service docker restart
+
+# Create repos directory
 sudo -u lana mkdir /home/lana/repos
+
+# Add SSH Keys
 sudo -u lana ssh-keygen -t rsa -C "lana@example.com"
 sudo -u lana cat /home/lana/.ssh/id_rsa.pub
 echo "Add the previous line to your the list of SSH keys for your git host account."
+
