07ef45c77720c9738459cf42b24f90e6763cbedd
Philip Woods <elzairthesorcerer@gmail.com>
2014-12-17 16:30:27 -0500

    Refactored input arguments

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var argv      = require('minimist')(process.argv.slice(2))
+var co        = require('co')
   , daemonize = require('daemonize2')
+  , fs        = require('co-fs')
+  , minimist  = require('minimist')
   , path      = require('path')
-  , task      = require(__dirname + '/../lib/task')
+  , task      = require(__dirname + '/lib/task')
   , util      = require('util')
   ;
 
@@ -13,47 +15,75 @@ var usage = util.format([
   , '\t-p --provider\tCode host that provides webhooks <github|bitbucket>'
   , '\t-r --repository\tName of repository'
   , '\t-u --url\tURL of private code repository'
-].join('\n'), argv['_'][0]);
+].join('\n'), process.argv.slice(0, 2).join(' '));
 
-var daemon = require('daemonize2').setup({
-    main: path.join(__dirname, '..', 'app.js')
-  , name: 'lana'
-  , pidfile: 'lana.pid'
-});
 
-var cmd = argv['_'][1];
-
-var addRepository = function*(args) {
-  var branches   = args['_'].slice(2)
-    , host       = args.h || args.host 
-    , language   = args.l || args.language || 'nodejs'
+var addRepository = function*(argv) {
+  var args = minimist(argv);
+  console.log(args);
+  var repository = args['_'][0]
+    , branch     = args['_'][1]
+    , hostStr    = args.H || args.host || '127.0.0.1'
+    , hosts      = hostStr.split(',')
+    , langStr    = args.l || args.language || 'nodejs'
+    , languages  = langStr.split(',')
     , provider   = args.p || args.provider || 'github'
-    , repository = args['_'][2]
-    , test       = true;
+    , tests      = args['_'].slice(2)
     , url        = args.u || args.url || ''
     ;
 
-  if (args.hasOwnProperty('t')) {
-    test = args.t;
-  }
-  else if (args.hasOwnProperty('test')) {
-    test = args.test;
+  var confPath = path.join(__dirname, 'conf', 'test.json');
+  var testConf = JSON.parse(yield fs.readFile(confPath));
+  for (var i = 0; i < languages.length; i++) {
+    if (testConf.hasOwnProperty(languages[i])) {
+      tests.push(testConf[languages[i]]);
+    }
+    else {
+      console.error(util.format('Invalid language: %s', languages[i]));
+    }
   }
 
-  yield task.addRepository(repository, branches, host, language, provider, test, url);
+  //yield task.addRepository(repository, branch, hosts, provider, tests, url);
+  console.log(tests);
 };
 
+co(function* () {
+  var argv = process.argv;
+  var usage = util.format([
+      'Usage: %s <start|stop|add> [AddOptions]'
+    , 'Add Options:'
+    , '\tName of repository'
+    , '\tName of branch'
+    , '\t-H --host\tSpecify list of remote hosts'
+    , '\t-l --language\tProgramming language of application'
+    , '\t-p --provider\tCode host that provides webhooks <github|bitbucket>'
+    , '\t-u --url\tURL of private code repository'
+  ].join('\n'), argv.slice(0, 2).join(' '));
+  console.log(argv);
 
-switch (cmd) {
-  case 'start':
-    daemon.start();
-    break;
-  case 'stop':
-    daemon.stop();
-    break;
-  case 'add':
-    addRepository(argv);
-    break;
-  default:
-    console.error(usage);
-}
+  var daemon = daemonize.setup({
+      main: path.join(__dirname, 'app.js')
+    , name: 'lana'
+    , pidfile: 'lana.pid'
+  });
+
+  var cmd = argv[2];
+  console.log(cmd);
+
+  switch (cmd) {
+    case 'start':
+      daemon.start();
+      break;
+    case 'stop':
+      daemon.stop();
+      break;
+    case 'add':
+      yield addRepository(argv.slice(3));
+      break;
+    case '-h':
+      console.log(usage);
+      break;
+    default:
+      console.error(usage);
+  }
+});
